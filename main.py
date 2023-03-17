8d18bb4d4e86f865bb245c5135a07f7aad05733a
Simon B <simon.birkholz@uni-ulm.de>
2023-03-17 14:38:09 +0100

    restructured main loop

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import torch.optim as optim
 import torch.utils.data as data
 import torch.nn as nn
 from tqdm import tqdm
+import wandb
+import argparse
+import json
+import os
+
 def train(model, optimizer, loss_fn, train_loader, val_loader, epochs=20, device='cpu'):
+    model.to(device)
     for epoch in range(epochs):
         training_loss = 0.0
         val_loss = 0.0
@@ -41,32 +47,56 @@ def train(model, optimizer, loss_fn, train_loader, val_loader, epochs=20, device
         #print(f'Epoch {epoch}, Training Loss: {training_loss:.2f}, Validation Loss: {val_loss:.2f}, Accurarcy: {(num_correct/num_examples):.2f}')
         print(f'Epoch {epoch}, Training Loss: {training_loss:.2f}')
 
+def learn(allparams):
 
-print(f'CUDA: {torch.cuda.is_available()}')
+    run = wandb.init(
+        project='shape-processing-rnns',
+        config=dict(params=allparams)
+    )
 
-
-if __name__ == '__main__':
     ds = get_imagenet('H:\datasets\imagenet')
 
-    batch_size = 64
+    batch_size = 1024
 
     train_data_loader = data.DataLoader(ds, batch_size=batch_size)
-    #val_data_loader = data.DataLoader(ds_val, batch_size=batch_size)
+    # val_data_loader = data.DataLoader(ds_val, batch_size=batch_size)
 
     simple_network = nn.Sequential(
-          nn.Conv2d(3,20,5),
-          nn.ReLU(),
-          nn.Conv2d(20,64,5),
-          nn.ReLU(),
-          nn.Flatten(),
-          nn.Linear(112896,256)
-        )
+        nn.Conv2d(3, 20, 5),
+        nn.ReLU(),
+        nn.Conv2d(20, 64, 5),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.Linear(112896, 256)
+    )
 
     adam = optim.AdamW(simple_network.parameters(), lr=0.001)
 
     loss = nn.CrossEntropyLoss()
 
-    train(simple_network,adam,loss,train_data_loader,None,20,'cuda')
+    train(simple_network, adam, loss, train_data_loader, None, 20, 'cuda')
+    run.finish()
+
+
+if __name__ == '__main__':
+    print(f'CUDA: {torch.cuda.is_available()}')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file', type=str, help='Config file for models')
+    parser.add_argument('--out', type=str, help='name for saving the model', default='test_run')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config_file):
+        raise ValueError("No config file found")
+    with open(args.config_file) as f:
+        config = json.load(f)
+    config['save_dir'] = args.out
+    allparams = config.copy()
+    learn(allparams)
+
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
