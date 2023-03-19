5df40887dd07162258c0f153971d97cea2d3cb82
Simon B <simon.birkholz@uni-ulm.de>
2023-03-19 21:52:34 +0100

    added initial version of ConvLSTM and ReciprocalGatedCell

--- models/cells.py
+++ models/cells.py
@@ -94,10 +94,111 @@ class ConvGruCell(torch.nn.Module):
 
         recalled = reset_gate * hx
         combined = torch.cat([input,recalled], dim=1)
-        
+
         hy = self.can(combined)
         # TODO support different activation functions
         hy = F.relu(hy)
 
         hy = (1 * update_gate) * hx + update_gate * hy
         return hy
+
+
+class ConvLSTMCell(torch.nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: KernelArg,
+                 bias: bool = True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.bias = bias
+        self.nonlinearity = 'relu'
+
+        # reset/forget gate
+        self.wf = nn.Conv2d(in_channels=in_channels,out_channels=out_channels, kernel_size=kernel_size)
+        self.uf = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.vf = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # input gate
+        self.wi = nn.Conv2d(in_channels=in_channels,out_channels=out_channels, kernel_size=kernel_size)
+        self.ui = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.vi = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # output gate
+        self.wo = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.uo = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.vo = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # state candidate
+        self.can = nn.Conv2d(in_channels=in_channels + out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+    def forward(self,input ,hidden_state=None):
+        # Inputs:
+        # input: of shape (batch_size, input_size,height_size, width_size)
+        # hx: of shape (batch_size, hidden_size,height_size, width_size)
+        # Outputs:
+        # hy: of shape (batch_size, hidden_size,height_size, width_size)
+
+        if hidden_state is None:
+            h_cur = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+            c_cur = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+        else:
+            h_cur, c_cur = hidden_state
+
+        # TODO important LSTM has the output gate on the new state candidate and with hadamard product
+
+        forget_gate = F.sigmoid(self.wf(input) + self.uf(h_cur) + self.vf(c_cur))
+        input_gate = F.sigmoid(self.wi(input) + self.ui(h_cur) + self.vi(c_cur))
+        output_gate = F.sigmoid(self.wo(input) + self.uo(h_cur) + self.vo(c_cur))
+
+        combined = torch.cat([input, h_cur],dim=1)
+
+        candidate = F.tanh(self.can(combined))
+
+        c_next = forget_gate * c_cur + input_gate * candidate
+
+        # TODO support different activation functions
+        h_next = output_gate * F.tanh(c_next)
+        return h_next, c_next
+
+
+class ReciprocalGatedCell(torch.nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: KernelArg,
+                 bias: bool = True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.bias = bias
+        self.nonlinearity = 'relu'
+
+        # output gating
+        self.wch = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.whh = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # memory gating
+        self.whc = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.wcc = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+    def forward(self,input ,hidden_state=None):
+
+        if hidden_state is None:
+            h_cur = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+            c_cur = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+        else:
+            h_cur, c_cur = hidden_state
+
+        # TODO support different activation functions
+
+        # output gating
+        h_next = (1 - F.sigmoid(self.wch(c_cur))) * input + (1 - F.sigmoid(self.whh(h_cur))) * h_cur
+        h_next = F.tanh(h_next)
+
+        # memory gating
+        c_next = (1 - F.sigmoid(self.whc(h_cur))) * input + (1 - F.sigmoid(self.wcc(c_cur))) * c_cur
+        c_next = F.tanh(c_next)
+
+        return h_next, c_next
