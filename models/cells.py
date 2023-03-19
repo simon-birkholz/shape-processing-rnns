8ab2832f407fddc03702c0a038af41a301853ebd
Simon B <simon.birkholz@uni-ulm.de>
2023-03-19 21:08:25 +0100

    added initial version of ConvGRU

--- models/cells.py
+++ models/cells.py
@@ -42,7 +42,7 @@ class ConvRNNCell(torch.nn.Module):
 
         self.h2h = nn.Conv2d(in_channels=out_channels,out_channels=out_channels,kernel_size=kernel_size)
 
-    def forward(self,input: ,hx=None):
+    def forward(self,input ,hx=None):
         # Inputs:
         # input: of shape (batch_size, input_size,height_size, width_size)
         # hx: of shape (batch_size, hidden_size,height_size, width_size)
@@ -52,6 +52,52 @@ class ConvRNNCell(torch.nn.Module):
         if hx is None:
             hx = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
         hy = (self.x2h(input) + self.h2h(hx))
+        # TODO support different activation functions
         hy = F.relu(hy)
         return hy
 
+class ConvGruCell(torch.nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: KernelArg,
+                 bias: bool = True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.bias = bias
+        self.nonlinearity = 'relu'
+
+        # reset gate
+        self.wr = nn.Conv2d(in_channels=in_channels,out_channels=out_channels, kernel_size=kernel_size)
+        self.ur = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # update gate
+        self.wz = nn.Conv2d(in_channels=in_channels,out_channels=out_channels, kernel_size=kernel_size)
+        self.uz = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        # state candidate
+        self.can = nn.Conv2d(in_channels=in_channels + out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+    def forward(self,input ,hx=None):
+        # Inputs:
+        # input: of shape (batch_size, input_size,height_size, width_size)
+        # hx: of shape (batch_size, hidden_size,height_size, width_size)
+        # Outputs:
+        # hy: of shape (batch_size, hidden_size,height_size, width_size)
+
+        if hx is None:
+            hx = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+
+        reset_gate = F.sigmoid(self.wr(input) + self.ur(hx))
+        update_gate = F.sigmoid(self.wz(input) + self.uz(hx))
+
+        recalled = reset_gate * hx
+        combined = torch.cat([input,recalled], dim=1)
+        
+        hy = self.can(combined)
+        # TODO support different activation functions
+        hy = F.relu(hy)
+
+        hy = (1 * update_gate) * hx + update_gate * hy
+        return hy
