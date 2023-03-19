0c247628223ca02b1921543feb0962590ac2bd2d
Simon B <simon.birkholz@uni-ulm.de>
2023-03-19 20:37:23 +0100

    added initial version of ConvRNN

--- models/cells.py
+++ models/cells.py
@@ -1,10 +1,11 @@
 from typing import Union, Sequence
 
 import torch
+import torch.nn as nn
 from torch.nn import Parameter
 from torch.nn.utils.rnn import PackedSequence
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 
 from .utils import _pair
 
@@ -15,18 +16,42 @@ class ConvRNNCell(torch.nn.Module):
                  in_channels: int,
                  out_channels: int,
                  kernel_size: KernelArg,
-                 bias: bool=True,
-                 ndim: int=2,
-                 stride: KernelArg=1,
-                 dilation: KernelArg=1):
+                 bias: bool=True):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.bias = bias
-        self.ndim = ndim
+        self.nonlinearity = 'relu'
+
+        self.ndim = 2
 
         ntuple = _pair
 
         self.kernel_size = ntuple(kernel_size)
-        self.stride = ntuple(stride)
-        self.dilation = ntuple(dilation)
+        #self.stride = ntuple(stride)
+        #self.dilation = ntuple(dilation)
+
+        #TODO there are multiple options for combining the hidden state and the input state
+        # 1. Concat beforehand and then convolution
+        # 2. Elementwise addition and then convolution (Hidden State and Input state would need the same dimensions
+        # 3. Two convolution and then elementwise addition
+        # In our formulas option 3 is used for all gates
+
+        # todo padding und bias
+        self.x2h = nn.Conv2d(in_channels=in_channels,out_channels=out_channels, kernel_size=kernel_size)
+
+        self.h2h = nn.Conv2d(in_channels=out_channels,out_channels=out_channels,kernel_size=kernel_size)
+
+    def forward(self,input: ,hx=None):
+        # Inputs:
+        # input: of shape (batch_size, input_size,height_size, width_size)
+        # hx: of shape (batch_size, hidden_size,height_size, width_size)
+        # Outputs:
+        # hy: of shape (batch_size, hidden_size,height_size, width_size)
+
+        if hx is None:
+            hx = Variable(input.new_zeros(input.size(0), self.out_channels, input.size(2), input.size(3)))
+        hy = (self.x2h(input) + self.h2h(hx))
+        hy = F.relu(hy)
+        return hy
+
