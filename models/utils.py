baf3528afa52df7723193cced0f88b7c11f6c3c0
Simon B <simon.birkholz@uni-ulm.de>
2023-03-27 00:30:14 +0200

    fixed wrong import of Iterable

--- models/utils.py
+++ models/utils.py
@@ -1,9 +1,9 @@
-import collections
+import collections.abc
 from itertools import repeat
 
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, collections.Iterable):
+        if isinstance(x, collections.abc.Iterable):
             return x
         return tuple(repeat(x,n))
     return parse
