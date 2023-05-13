72a73a271ae914e32bdce49ccb2d1b292066c57c
Simon B <simon.birkholz@uni-ulm.de>
2023-05-14 01:30:51 +0200

    added documentation to diagnostic_stimuli transforms

--- experiments/diagnostic_stimuli.py
+++ experiments/diagnostic_stimuli.py
@@ -1,14 +1,12 @@
 from datasets.pascal_voc import PascalVoc
 
 import torch
-import torchvision.transforms as tfs
-import torchvision.transforms.functional as F
 
-import torch.nn.functional as TF
+import torch.nn.functional as F
 import torch.utils.data as data
 
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+
 import numpy as np
 import argparse
 
@@ -17,180 +15,8 @@ from typing import List
 from datasets.imagenet_classes import get_imagenet_class_mapping
 from models.architecture import FeedForwardTower
 
-from scipy.ndimage import binary_dilation, gaussian_filter
+from ds_transforms import NORMAL, FOREGROUND, SHILOUETTE, FRANKENSTEIN, SERRATED
 
-
-class FillOutMask:
-    def __init__(self, inverted=False, fill=0):
-        self.fill = (fill, fill, fill)
-        self.inverted = inverted
-
-    def __call__(self, image, bbox, mask):
-        image_array = np.array(image).astype(np.uint8)
-        mask_array = np.array(mask).astype(np.uint8)
-
-        if not self.inverted:
-            target_color = (255, 255, 255)
-        else:
-            target_color = (0, 0, 0)
-
-        mask_indices = np.where((mask_array == target_color).all(axis=-1))
-        image_array[mask_indices] = self.fill
-
-        return F.to_pil_image(image_array), bbox, mask
-
-
-class DiscardMaskAndBox():
-    def __call__(self, image, bbox, mask):
-        return image
-
-
-class EnlargeAndCropBoundingBox:
-    def __init__(self, factor=1.4):
-        self.factor = factor
-
-    def __call__(self, image, bbox):
-        x, y, w, h = bbox
-
-        new_w = w * self.factor
-        new_h = h * self.factor
-
-        new_x = max(0, x - (new_w - w) / 2)
-        new_y = max(0, y - (new_h - h) / 2)
-
-        # Make sure the new bounding box is within the image boundaries
-        img_width, img_height = image.size
-        new_x = min(new_x, img_width - new_w)
-        new_y = min(new_y, img_height - new_h)
-
-        # Crop the image
-        cropped_image = F.crop(image, int(new_y), int(new_x), int(new_h), int(new_w))
-
-        # Update the bounding box coordinates in the cropped image
-        updated_bbox = torch.tensor([x - new_x, y - new_y, w, h], dtype=torch.float32)
-
-        return cropped_image, updated_bbox
-
-
-class EnlargeImageAndMask():
-    def __init__(self, factor=1.4):
-        self.enlarge_tfs = EnlargeAndCropBoundingBox(factor=factor)
-
-    def __call__(self, image, bbox, mask):
-        new_img, new_bbox = self.enlarge_tfs(image, bbox)
-        new_mask, _, = self.enlarge_tfs(mask, bbox)
-        return new_img, new_bbox, new_mask
-
-
-class FrankensteinFlip:
-    def __call__(self, image):
-        image_array = np.array(image)
-
-        upper, lower = np.split(image_array, 2)
-        lower = np.flip(lower, axis=1)
-        image_array = np.vstack((upper, lower))
-
-        left, right = np.split(image_array, 2, axis=1)
-        right = np.flip(right)
-        image_array = np.hstack((left, right))
-
-        return F.to_pil_image(image_array)
-
-
-class SerratedDilation:
-    def __init__(self, borderwidth=5, sigma=2.0):
-        self.borderwidth = borderwidth
-        self.sigma = sigma
-
-    def __call__(self, image, bbox, mask):
-        image_array = np.average(np.array(image), axis=-1)
-        mask_array = np.average(np.array(mask), axis=-1)
-
-        x, y, w, h = bbox
-
-        dilated = binary_dilation(mask_array, iterations=self.borderwidth)
-
-        noise = np.random.randn(image_array.shape[0], image_array.shape[1])
-        noise = (gaussian_filter(noise, sigma=self.sigma) > 0.0) * 255
-
-        result = (255 - mask_array) * (1 - dilated) + dilated * noise
-        result = 255 - ((255 - image_array) + (255 - result))
-        result = result.astype(np.uint8)
-        result = np.stack((result, result, result), axis=2)
-        return F.to_pil_image(result), bbox, mask
-
-
-class MyCompose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, img):
-        state = img
-        for t in self.transforms:
-            state = t(*state)
-        return state
-
-
-def plot_16_images(ds, title):
-    fig = plt.figure(figsize=(40, 40))
-    columns = 4
-    rows = 4
-    for i in range(1, columns * rows + 1):
-        img = ds[i][0].movedim(0, -1)
-        # bnd = ds[i][0][1]
-        # rect = patches.Rectangle((bnd[0], bnd[1]), bnd[2], bnd[3], linewidth=1, edgecolor='r', facecolor='none')
-        ax = fig.add_subplot(rows, columns, i)
-        plt.imshow(img)
-        # ax.add_patch(rect)
-        plt.axis('off')
-
-    plt.title(title)
-    plt.savefig('example-images.png')
-
-
-tfs_normal = tfs.Compose([MyCompose([
-    EnlargeImageAndMask(),
-    DiscardMaskAndBox()
-]),
-    tfs.Resize((224, 224)),
-    tfs.ToTensor()])
-
-tfs_foreground = tfs.Compose([MyCompose([
-    EnlargeImageAndMask(),
-    FillOutMask(inverted=True, fill=255),
-    DiscardMaskAndBox()
-]),
-    tfs.Resize((224, 224)),
-    tfs.ToTensor()])
-
-tfs_shilouette = tfs.Compose([MyCompose([
-    EnlargeImageAndMask(),
-    FillOutMask(inverted=True, fill=255),
-    FillOutMask(inverted=False, fill=0),
-    DiscardMaskAndBox()
-]),
-    tfs.Resize((224, 224)),
-    tfs.ToTensor()])
-
-tfs_frankenstein = tfs.Compose([MyCompose([
-    EnlargeImageAndMask(),
-    FillOutMask(inverted=True, fill=255),
-    FillOutMask(inverted=False, fill=0),
-    DiscardMaskAndBox()
-]),
-    tfs.Resize((224, 224)),
-    FrankensteinFlip(),
-    tfs.ToTensor()])
-
-tfs_serrated = tfs.Compose([MyCompose([
-    EnlargeImageAndMask(),
-    FillOutMask(inverted=True, fill=255),
-    FillOutMask(inverted=False, fill=0),
-    SerratedDilation(),
-    DiscardMaskAndBox()
-]),
-    tfs.Resize((224, 224)),
-    tfs.ToTensor()])
 
 
 def permutation_test(probs, labels, n=1000):
@@ -221,7 +47,7 @@ def classify(model,
         all_labels = torch.cat((all_labels, targets), dim=0)
 
         outputs = model(inputs)
-        probabilities = TF.softmax(outputs, dim=1)
+        probabilities = F.softmax(outputs, dim=1)
         predicted = torch.argmax(probabilities, dim=1)
 
         voc_predicted = torch.as_tensor([imagenet2voc[p] for p in predicted], dtype=torch.int32)
@@ -254,11 +80,11 @@ def main(
         out_file: str,
         batch_size: int
 ):
-    normal_ds = PascalVoc(dataset_path, set, transform=tfs_normal)
-    foreground_ds = PascalVoc(dataset_path, set, transform=tfs_foreground)
-    shilouette_ds = PascalVoc(dataset_path, set, transform=tfs_shilouette)
-    frankenstein_ds = PascalVoc(dataset_path, set, transform=tfs_frankenstein)
-    serrated_ds = PascalVoc(dataset_path, set, transform=tfs_serrated)
+    normal_ds = PascalVoc(dataset_path, set, transform=NORMAL)
+    foreground_ds = PascalVoc(dataset_path, set, transform=FOREGROUND)
+    shilouette_ds = PascalVoc(dataset_path, set, transform=SHILOUETTE)
+    frankenstein_ds = PascalVoc(dataset_path, set, transform=FRANKENSTEIN)
+    serrated_ds = PascalVoc(dataset_path, set, transform=SERRATED)
 
     all_datasets = [(normal_ds, 'normal'), (foreground_ds, 'foreground'), (shilouette_ds, 'shilouette'),
                     (frankenstein_ds, 'frankenstein'), (serrated_ds, 'serrated')]
